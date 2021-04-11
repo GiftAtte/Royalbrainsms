feb368554be585216fa10adefb92fce975f4a891
GiftAtte <attegift@gmail.com>
2021-04-11 16:50:46 +0100

    positions correctedffkk

--- app/Http/Controllers/API/ScoreController.php
+++ app/Http/Controllers/API/ScoreController.php
@@ -431,191 +431,166 @@ return ["grade"=>'F',"narration"=>'','credit_point'=>0,'total'=>0];
     }
 
 
+    public function getSubjectRank($student_id,$report_id,$subject_id,$arm_id=null){
 
-    public function getRank($score,$scores){
-        //$collection=collect($score)->sortDesc();
-        rsort($scores);
-     $collection=collect($scores)->sortDesc()->sortBy('total');
-    $position=$collection->where('total',$score)->keys();
-           if(!empty($position[0])){
-         return $this->ordinal(($position[0]+1));
-}
-
-    //   $rank=1;
-    //   $rankArr=array();
-    //   $positions=null;
-    //                   if($arm_id){
-    //                     $positions = DB::table('marks')
-    //               ->where( [
-    //                  ['subject_id',$subject_id],
-    //                  ['report_id',$report_id],
-    //                  ['arm_id',$arm_id]
-    //                  ])->select('student_id','subject_id',"total")->orderBy("total",'desc')
-    //               ->get();
-    //                   }else{
-    //              $positions = DB::table('marks')
-    //               ->where( [
-    //                  ['subject_id',$subject_id],
-    //                  ['report_id',$report_id]
-    //                  ])->select('student_id','subject_id',"total")->orderBy("total",'desc')
-    //               ->get();
-    //                   }
-    //           //   return  $this-> Rank($positions,$student_id) ;
-    //     array_push($rankArr,$this->Rank($positions,$student_id));
+        $rank=1;
+        $rankArr=array();
+         $positions=null;
+                        if($arm_id){
+                          $positions = DB::table('marks')
+                    ->where( [
+                       ['subject_id',$subject_id],
+                       ['report_id',$report_id],
+                       ['arm_id',$arm_id]
+                       ])->select('student_id','subject_id',"total")->orderBy("total",'desc')
+                    ->get();
+                        }else{
+                   $positions = DB::table('marks')
+                    ->where( [
+                       ['subject_id',$subject_id],
+                       ['report_id',$report_id]
+                       ])->select('student_id','subject_id',"total")->orderBy("total",'desc')
+                    ->get();
+                        }
+                 //   return  $this-> Rank($positions,$student_id) ;
+          array_push($rankArr,$this->Rank($positions,$student_id));
 
 
 
 
- return '';
-}
-
-
-
-public function Rank($positions=array() ,$student_id){
-                    $rank=1;
-                 $total=0;
-                 $rank2=0;
-                 $counter=1;
-                   $num=1;
-                //  return[$positions,$student_id] ;
-                foreach($positions as $position) {
-
-
-                        if($position->total==$total)
-                        {    $rank=$rank-$num;$rank2=$rank ;$num=++$num;}
-
-
-                        if($position->student_id==$student_id){
-                          return ['subject_id'=>$position->subject_id,'position'=>$this->ordinal($rank),'student_id'=>$position->student_id];
-                          break;
-                              }
-
-
-      $total=$position->total;
-      if($rank2==$rank){$rank=$rank+$num;}else{
-          $num=1;
-          $rank=++$rank;
-      }
-
-
+   return $this->Rank($positions,$student_id);
   }
 
 
-  }
+
+  public function Rank($positions=array() ,$student_id){
+                      $rank=1;
+                   $total=0;
+                   $rank2=0;
+                   $counter=1;
+                     $num=1;
+                  //  return[$positions,$student_id] ;
+                  foreach($positions as $position) {
 
 
-public function Rank2($positions=array() ,$id){
-                 $rank=1;
-                 $total=0;
-                 $rank2=0;
-                 $counter=1;
-                   $num=1;
-                foreach($positions as $position) {
+                          if($position->total==$total)
+                          {    $rank=$rank-$num;$rank2=$rank ;$num=++$num;}
 
 
-                        if($position->total==$total)
-                        {    $rank=$rank-$num;$rank2=$rank ;$num=++$num;}
+                          if($position->student_id==$student_id){
+                            return ['subject_id'=>$position->subject_id,'position'=>$this->ordinal($rank),'student_id'=>$position->student_id];
+                            break;
+                                }
 
 
-                        if($position->student_id==$id){
-                          return [$position->student_id,$this->ordinal($rank),$position->total];
-                          break;
-                              }
+        $total=$position->total;
+        if($rank2==$rank){$rank=$rank+$num;}else{
+            $num=1;
+            $rank=++$rank;
+        }
 
 
-      $total=$position->total;
-      if($rank2==$rank){$rank=$rank+$num;}else{
-          $num=1;
-          $rank=++$rank;
-      }
-
-
-  }
-
-
-  }
-
-  // Class and arm positioning ===sorting students
-public function studentPosition($id,$report_id,$arm=false){
-$report=Report::findOrFail($report_id);
-$student=Student::findOrFail($id);
-$students=null;
-if($arm){
-  $students=Student::select('id')->where([['class_id',$report->level_id],['arm_id',$student->arm_id]])->get();
-}
-else{
-$students=Student::select('id')->where('class_id',$report->level_id)->get();
-}
-$studentArr=array();
-$studentPosition=array();
-foreach($students as $student){
-$avgScores=DB::table('marks')->whereNotIn('total',[0])
--> where([['student_id',$student->id],['type','Academic'],
-['report_id',$report_id]
-
-])
-->select(DB::raw('avg(total) as total'))
-->first();
-
-
-if(!empty($avgScores->total)){
-array_push($studentArr,['total'=>$avgScores->total]);
-}
-}
-$score=DB::table('marks')->whereNotIn('total',[0])
--> where([['student_id',$id],['type','Academic'],
-['report_id',$report_id]
-
-])
-->avg('total');
-//rsort($studentArr);
-// foreach($students as $student){
-//   array_push($studentPosition,  $this->Rank3($studentArr,$id));
-// }
- //return collect($studentArr)->where('Total',158.666666666666657192763523198664188385009765625)->keys();
-return $this->getRank($score,$studentArr);
-}
-
-// class positionioning/arm positioning Ranking
-public function Rank3($positions=array() ,$id){
-                 $rank=1;
-                 $total=0;
-                  $rank2=0;
-                  $counter=1;
-
-                foreach($positions as $position) {
-
-
-                        if($position->Total==$total)
-                        {    $rank=$rank-$counter; $rank2=$rank;$counter=++$counter;}
-
-
-                        if($position->student_id==$id){
-                          return$this->ordinal($rank);
-                          break;
-                              }
-
-
-      $total=$position->Total;
-      if($rank2==$rank){$rank=$rank+$counter;}else{
-   $rank=++$rank;
-   $counter=1;
-      }
-
-  }
-
-
-  }
-  public function ordinal($number)
-{
-    $ends = array('th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th');
-    if ((($number % 100) >= 11) && (($number % 100) <= 13)) {
-        return $number . 'th';
-    } else {
-        return $number . $ends[$number % 10];
     }
-}
 
+
+    }
+
+
+  public function Rank2($positions=array() ,$id){
+                   $rank=1;
+                   $total=0;
+                   $rank2=0;
+                   $counter=1;
+                     $num=1;
+                  foreach($positions as $position) {
+
+
+                          if($position->total==$total)
+                          {    $rank=$rank-$num;$rank2=$rank ;$num=++$num;}
+
+
+                          if($position->student_id==$id){
+                            return [$position->student_id,$this->ordinal($rank),$position->total];
+                            break;
+                                }
+
+
+        $total=$position->total;
+        if($rank2==$rank){$rank=$rank+$num;}else{
+            $num=1;
+            $rank=++$rank;
+        }
+
+
+    }
+
+
+    }
+
+    // Class and arm positioning ===sorting students
+  public function studentPosition($id,$report_id,$arm=false){
+  $report=Report::findOrFail($report_id);
+  $student=Student::findOrFail($id);
+  $students=null;
+  if($arm){
+    $students=Student::select('id')->where([['class_id',$report->level_id],['arm_id',$student->arm_id]])->get();
+  }
+  else{
+  $students=Student::select('id')->where('class_id',$report->level_id)->get();
+  }
+  $studentArr=array();
+  $studentPosition=array();
+  foreach($students as $student){
+  $avgScores=DB::table('marks')->whereNotIn('total',[0])
+  -> where([['student_id',$student->id],['type','Academic'],
+  ['report_id',$report_id]
+
+  ])
+  ->select(DB::raw('avg(total) as Total'),'student_id')
+
+   ->groupBy('student_id')
+  ->get();
+
+  foreach($avgScores as $avgScore){
+  array_push($studentArr,$avgScore);
+     rsort($studentArr);
+  }}
+  // foreach($students as $student){
+  //   array_push($studentPosition,  $this->Rank3($studentArr,$id));
+  // }
+  return $this->Rank3($studentArr,$id);
+  }
+
+  // class positionioning/arm positioning Ranking
+  public function Rank3($positions=array() ,$id){
+                   $rank=1;
+                   $total=0;
+                    $rank2=0;
+                    $counter=1;
+
+                  foreach($positions as $position) {
+
+
+                          if($position->Total==$total)
+                          {    $rank=$rank-$counter; $rank2=$rank;$counter=++$counter;}
+
+
+                          if($position->student_id==$id){
+                            return$this->ordinal($rank);
+                            break;
+                                }
+
+
+        $total=$position->Total;
+        if($rank2==$rank){$rank=$rank+$counter;}else{
+     $rank=++$rank;
+     $counter=1;
+        }
+
+    }
+
+
+    }
 
 public function score_template($report_id,$subject_id){
     $subject=Subject::findOrFail($subject_id);
