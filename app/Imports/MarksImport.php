4c7d1a0603650a5c98810b3e9b4e28a05ed23a9a
GiftAtte <attegift@gmail.com>
2021-05-28 21:21:40 +0100

    total cal

--- app/Imports/MarksImport.php
+++ app/Imports/MarksImport.php
@@ -22,7 +22,7 @@ class MarksImport implements ToModel, WithHeadingRow
         Mark::where([['subject_id',$row['subjects_id']],['report_id',$row['report_id']],['student_id',$row['student_id']]])->delete();
         $scoreController =new ScoreController();
        $report=Report::findOrFail($row['report_id']);
-        $total=$scoreController->sum($row['ca1'],$row['ca2'],['ca3'],$row['exams']);
+        $total=$scoreController->default_sum($row['ca1'],$row['ca2'],['ca3'],$row['exams']);
         $gradding=$scoreController->grade($total,$report->gradinggroup_id,auth('api')->user()->school_id);
 
         $subject_type=Level_sub::where([['level_id',$row['level_id']],['subject_id',$row['subjects_id']]])->first();
