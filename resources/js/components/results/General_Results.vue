0f3fd384cf42f4500ec2640bce4b6d70a96fc51f
GiftAtte <attegift@gmail.com>
2021-07-14 11:01:38 +0100

    default terminal

--- resources/js/components/results/General_Results.vue
+++ resources/js/components/results/General_Results.vue
@@ -107,14 +107,20 @@
 <td v-show="isTest2">{{score.test2}}</td>
 <td v-show="isTest3">{{score.test3}}</td>
 <td>{{score.exams?score.exams:'-'}}</td>
-<td  v-show="isMidterm">{{score.mid_term?score.mid_term:'-'}}</td>
+<!-- <td  v-show="isMidterm">{{score.mid_term?score.mid_term:'-'}}</td> -->
 <td>{{score.total?score.total:'-'}}</td>
-<td v-show="isThird_term && isCummulative"  v-for="total in Total"  v-if="(total.subject_id===score.subject_id && total.term_id===1)">
-<div>{{total.total?total.total:'-'}}</div></td>
-<td v-show="isThird_term && isCummulative"   v-for="total in Total"  v-if="(total.subject_id===score.subject_id && total.term_id===2)">
-<div>{{total.total?total.total:'-'}}</div></td>
-<td v-show="isThird_term && isCummulative" >{{score.grand_total?score.grand_total:''}}</td>
-<td v-show="isThird_term && isCummulative" >{{score.average?score.average:''}}</td>
+<td v-show="isThird_term && isCummulative" >
+<div v-for="total in Total"  v-if="(total.subject_id===score.subject_id && total.term_id===1)">
+    {{total.total?total.total:'-'}}
+</div>
+</td>
+<td v-show="isThird_term && isCummulative" >
+<div v-for="total in Total"  v-if="(total.subject_id===score.subject_id && total.term_id===2)">
+    {{total.total?total.total:'-'}}
+</div>
+</td>
+<td v-show="isThird_term && isCummulative" >{{score.grand_total?score.grand_total:'-'}}</td>
+<td v-show="isThird_term && isCummulative" >{{score.average?score.average:'-'}}</td>
 
 <td v-show="isThird_term && isCummulative" >{{score.cummulative_grade?score.cummulative_grade:'-'}}</td>
 <td v-show="isThird_term && isCummulative" >{{score.cummulative_narration?score.cummulative_narration:'-'}}</td>
@@ -148,10 +154,17 @@
 <td>{{score.exams?score.exams:''}}</td>
 <td  v-show="isMidterm">{{score.mid_term?score.mid_term:''}}</td>
 <td>{{score.total?score.total:'-'}}</td>
-<td >
-<div v-show="isThird_term && isCummulative"  v-for="total in Total"  v-if="(total.subject_id===score.subject_id && total.term_id===1)">{{total.total?total.total:'-'}}{{''}}</div></td>
-<td >
-<div v-show="isThird_term && isCummulative"   v-for="total in Total"  v-if="(total.subject_id===score.subject_id && total.term_id===2)">{{total?total.total:'-'}}{{''}}</div></td>
+<td v-show="isThird_term && isCummulative" >
+<div v-for="total in Total"  v-if="(total.subject_id===score.subject_id && total.term_id===1)">
+    {{total.total?total.total:'-'}}
+</div>
+</td>
+<td v-show="isThird_term && isCummulative" >
+<div v-for="total in Total"  v-if="(total.subject_id===score.subject_id && total.term_id===2)">
+    {{total.total?total.total:'-'}}
+</div>
+</td>
+
 
 <td v-show="isThird_term && isCummulative" >{{score.grand_total?score.grand_total:'-'}}</td>
 <td v-show="isThird_term && isCummulative" >{{score.average?score.average:'-'}}</td>
