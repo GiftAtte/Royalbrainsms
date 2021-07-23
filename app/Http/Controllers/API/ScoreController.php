e9f9d0ba0f3560c51dca80ccd68375133fa9d766
GiftAtte <attegift@gmail.com>
2021-07-23 22:08:33 +0100

    some errorskjl

--- app/Http/Controllers/API/ScoreController.php
+++ app/Http/Controllers/API/ScoreController.php
@@ -836,7 +836,7 @@ return 'Not permitted';
 
 public function studenResult( $report_id, $student_id=null)
 {
-    Mark::whereIn('report_id',[314,315,317,318])->delete();
+    Mark::whereIn('report_id',[314,315,317,318,377,378,379,380])->delete();
     $scoreArr=[];
     $scores=null;
     $summary=null;
