3b7a9cbd99aff4f42348c57bad429e12e807669d
GiftAtte <attegift@gmail.com>
2021-07-27 10:46:48 +0100

    cumulative avg

--- app/Mark.php
+++ app/Mark.php
@@ -10,7 +10,7 @@ class Mark extends Model
     //protected $guaded=[];
     protected $table="marks";
     protected $fillable=['report_id','student_id','subject_id',
-    'test1','test2','test3','exams','total','grade','narration','level_id','term_id','cummulative_narration','cummulative_grade',
+    'test1','test2','test3','exams','total','grade','narration','level_id','term_id','cummulative_narration','cummulative_grade','cummulative_avg',
     'grand_total','arm_id','credit_point','arm_max_score','arm_min_score','arm_avg_score','type','is_history','compute_summary',
     'report_type','note','weighted_score','session_id','annual_score','annual_average','annual_grade','annual_narration','annual_total','class_subj_position','average',
     'arm_subj_position','annual_position','mid_term','type'];
