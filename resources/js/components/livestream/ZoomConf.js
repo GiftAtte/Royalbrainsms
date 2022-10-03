14cb24b5600fbcb1d72e87dc3e813a2b524dcf48
GiftAtte <attegift@gmail.com>
2022-10-03 08:20:14 +0100

    Zoom Integrations

--- resources/js/components/livestream/ZoomConf.js
+++ resources/js/components/livestream/ZoomConf.js
@@ -1,6 +1,5 @@
 export const CLIENT_ID = "YNuMefhRguk_QIAv7feg";
-export const REDIRECT_URI =
-    "https://portal.royalbrainsms.com/zoomoauthredirect";
+export const REDIRECT_URI = "https://portal.royalbrainsms.com/zoomoauthredirect";
 export const LEAVE_URL = "http://localhost:8000/liveClasses";
 export const SIGNATURE_ENTRY_ENDPOINT = "/api/livestream/zoomZignature";
 export const ZOOM_AUTHORIZE_URL = `https://zoom.us/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
